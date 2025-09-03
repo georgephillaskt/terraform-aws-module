98efdfcd56c15284cf8840dbdb1a652d666bb0fe
georgephillaskt <georgephillas.kt@gmail.com>
2025-09-03 09:47:50 +0530

    Update output.tf

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,1 +1,9 @@
+output "instance_ids" {
+  description = "IDs of the EC2 instances"
+  value       = aws_instance.ec2_instance[*].id
+}
 
+output "security_group_id" {
+  description = "ID of the security group"
+  value       = aws_security_group.instance_sg.id
+}
