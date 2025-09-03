d9c27c654416d45a9d8360012167551b86fbe25d
georgephillaskt <georgephillas.kt@gmail.com>
2025-09-03 09:48:55 +0530

    Update variables.tf

--- modules/vpc/aws-vpc-3-tier/variables.tf
+++ modules/vpc/aws-vpc-3-tier/variables.tf
@@ -1,1 +1,36 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
 
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "environment" {
+  description = "Environment (e.g., dev, qa, prod)"
+  type        = string
+}
+
+variable "customer" {
+  description = "Customer name"
+  type        = string
+}
+
+variable "product" {
+  description = "Product name"
+  type        = string
+}
+
+variable "subnet_cidrs" {
+  description = "CIDR blocks for subnets"
+  type = object({
+    dmz_a = string
+    dmz_b = string
+    app_a = string
+    app_b = string
+    db_a  = string
+    db_b  = string
+  })
+}
